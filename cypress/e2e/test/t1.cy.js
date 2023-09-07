eefff38dd7ed9cd23c8575ffce4da2c10e9d1d7e
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2023-09-07 13:51:52 +0300

    added failed tests

--- cypress/e2e/test/t1.cy.js
+++ cypress/e2e/test/t1.cy.js
@@ -1,5 +1,6 @@
 describe('test1', () => {
   it('test1 nonParallel', { tags: ['testSmoke', 'nonParallel'] }, () => {
     cy.wait(1000);
+    expect(true).to.be.equal(false);
   });
 });
--- cypress/e2e/test/t4.cy.js
+++ cypress/e2e/test/t4.cy.js
@@ -1,5 +1,6 @@
 describe('test4', () => {
   it('test4 parallel', { tags: ['testSmoke'] }, () => {
     cy.wait(1000);
+    expect(true).to.be.equal(false);
   });
 });
