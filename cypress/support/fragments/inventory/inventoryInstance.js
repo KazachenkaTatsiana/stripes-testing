a352b09e929f931a85a3130f156b42900e664522
Victor Bystrov <bystrov1988@gmail.com>
2023-10-26 00:13:35 +0300

    C375259

--- cypress/support/fragments/inventory/inventoryInstance.js
+++ cypress/support/fragments/inventory/inventoryInstance.js
@@ -222,6 +222,15 @@ const verifySeriesStatement = (indexRow, value) => {
   );
 };
 
+const verifySubjectHeading = (value) => {
+  cy.expect(
+    Accordion('Subject')
+      .find(MultiColumnList({ id: 'list-subject' }))
+      .find(MultiColumnListCell({ content: value }))
+      .exists(),
+  );
+};
+
 const verifyContributor = (indexRow, indexColumn, value) => {
   cy.expect(
     contributorAccordion
@@ -288,6 +297,7 @@ export default {
   openItemByBarcode,
   verifyAlternativeTitle,
   verifySeriesStatement,
+  verifySubjectHeading,
   verifyContributor,
   verifyContributorWithMarcAppLink,
 
